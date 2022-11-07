6435791549ae21772793b894c594f2900ade8f8d
Viet Nhien Tran Duc <vtranduc@Viets-MacBook-Pro.local>
2022-11-07 02:50:07 -0500

    spread out the buttons

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { DragAndDrop } from "./view";
 import "babylonjs-loaders";
 import { useDispatch } from "react-redux";
 import { clearAllMeshes, setPreset } from "./reducer";
+import { CSSProperties } from "react";
+
+const buttonStyle: CSSProperties = { margin: "10px 10px" };
 
 function App() {
   const dispatch = useDispatch();
@@ -14,20 +17,30 @@ function App() {
         id={ElementId.RenderCanvas}
       />
 
-      <button onClick={() => dispatch(clearAllMeshes())}>CLEAR ALL</button>
-      <button onClick={() => dispatch(setPreset(ScenePreset.PrimitiveObjects))}>
-        Primitive preset
-      </button>
-      <button onClick={() => dispatch(setPreset(ScenePreset.Miqote))}>
-        Miqo'te preset
-      </button>
+      <div style={{ margin: "10px 0px" }}>
+        <button onClick={() => dispatch(clearAllMeshes())} style={buttonStyle}>
+          CLEAR ALL
+        </button>
+        <button
+          onClick={() => dispatch(setPreset(ScenePreset.PrimitiveObjects))}
+          style={buttonStyle}
+        >
+          Primitive preset
+        </button>
+        <button
+          onClick={() => dispatch(setPreset(ScenePreset.Miqote))}
+          style={buttonStyle}
+        >
+          Miqo'te preset
+        </button>
 
-      <h3>How to use</h3>
+        <h3>How to use</h3>
 
-      <p>
-        Drag and drop and glb file or folder containing gltf and all of its
-        dependencies to view them!
-      </p>
+        <p>
+          Drag and drop and glb file or folder containing gltf and all of its
+          dependencies to view them!
+        </p>
+      </div>
     </>
   );
 }
